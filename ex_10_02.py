40cd7fa7118e4a8262a4fad8d0e67a1fd7d45347
Vanja Tanevski <vanja_tanevski@hotmail.com>
2019-04-04 12:33:50 +0200

    ex_10_02

--- ex_10_02.py
+++ ex_10_02.py
@@ -3,7 +3,7 @@
 # splitting the string a second time using a colon.
 # "From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008"
 # Once you have accumulated the counts for each hour, print out the counts, sorted by hour as shown below.
-
+# ______________________________________________________________________________________________________________________
 
 # First, with while loop and try and except we assure to get the right file name avoiding Traceback
 while True:
